aec2cbd786d19ced5c669db07cdc71cacf62dc2a
Fredev01 <fredev08@gmail.com>
2025-03-19 09:16:53 -0600

    Added models and repository

---
+++ app/features/workspaces/domain/model.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, ValidationError, field_validator
+
+class Workspace(BaseModel):
+    name: str
+    owner: str
+
+class WorkspaceCreate(Workspace):
+    pass
+    @field_validator('name')
+    @classmethod
+    def validate_name(cls, value: str):
+        if len(value) < 3:
+            raise ValueError(
+                "El nombre del workspace debe tener al menos 3 caracteres.")
+        return value
+
+class WorkspaceResponse(Workspace):
+    id: str
