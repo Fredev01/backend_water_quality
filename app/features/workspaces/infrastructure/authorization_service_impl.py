c428812895d5e0020d9ba46fbff9fc78612aa15a
Fredev01 <fredev08@gmail.com>
2025-04-06 13:46:08 -0600

    Fix: changed message when workspace dosen't exist.

--- app/features/workspaces/infrastructure/authorization_service_impl.py
+++ app/features/workspaces/infrastructure/authorization_service_impl.py
@@ -4,10 +4,9 @@ class WorkspaceAuthorizationServiceImpl:
         """Check if the user has access to the workspace."""
         workspace_ref = db.reference("workspaces")
         workspace_data = workspace_ref.child(workspace_id).get()
-        return workspace_data and (
-            workspace_data.get("owner") == user_id or
-            any(user_id == g.get("email") for g in workspace_data.get("guests", {}).values())
-        )
+        if workspace_data is None:
+            raise ValueError(f"Workspace with ID {workspace_id} does not exist.")
+        return workspace_data.get("owner") == user_id or any(user_id == g.get("email") for g in workspace_data.get("guests", {}).values())
     
     def get_user_role(self, workspace_id: str, user_id: str) -> str:
         pass
