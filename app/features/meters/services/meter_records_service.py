72a1c73596c8a038c9b1305eb6f7fcfd972e89fa
Fredev01 <fredev08@gmail.com>
2025-04-06 13:52:42 -0600

    Added method for get the records by sensor.

--- app/features/meters/services/meter_records_service.py
+++ app/features/meters/services/meter_records_service.py
@@ -1,6 +1,7 @@
-from app.features.meters.domain.model import SensorIdentifier
+from app.features.meters.domain.model import SensorIdentifier, SensorQueryParams
 from app.features.meters.domain.repository import MeterRecordsRepository
 from app.features.workspaces.domain.services import WorkspaceAuthorizationService
+from app.share.socketio.domain.model import Record
 
 
 class MeterRecordsService:
@@ -15,7 +16,17 @@ class MeterRecordsService:
         )
         
         if not has_access:
-            raise PermissionError("User does not have access to this meter")
+            raise ValueError("meter not found")
         
         # Obtener datos
         return self.repository.get_10_last_sensor_records(identifier.workspace_id, identifier.meter_id)
+    
+    def get_sensor_records(self, identifier: SensorIdentifier, params: SensorQueryParams | None = None)->list[Record]:
+        has_access = self.auth_service.check_user_access(
+            identifier.workspace_id, 
+            identifier.user_id
+        )
+        
+        if not has_access:
+            raise ValueError("meter not found")
+        return self.repository.get_sensor_records(identifier, params)
