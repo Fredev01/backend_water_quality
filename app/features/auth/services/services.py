905709ee2da09c6ce84d042b127b1ad346117185
EmmanuelOZ <davidozuna84@hotmail.com>
2025-06-05 10:29:47 -0600

    feat: recuperar contraseña, flujo de generar una código, validarlo y actualizar la contraseña. TODO: agregar el servicio de mandar emails

--- app/features/auth/services/services.py
+++ app/features/auth/services/services.py
@@ -1,6 +1,10 @@
 from fastapi import HTTPException
-from firebase_admin import auth
+import random
+from datetime import datetime, timedelta
+from firebase_admin import auth, db
 import httpx
+from app.features.auth.domain.body import UpdatePassword
+from app.features.auth.domain.model import VerifyResetCode
 from app.share.firebase.domain.config import FirebaseConfigImpl
 from app.share.users.domain.enum.roles import Roles
 from app.share.users.domain.model.auth import UserLogin, UserRegister
@@ -66,3 +70,56 @@ class AuthService:
                     status_code=400, detail="Failed to send reset email")
 
             return {"message": "Password reset email sent"}
+
+    async def generate_verification_code(self, email: str) -> int:
+        user = self.user_repo.get_by_email(email)
+        if user is None:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        reset_code = random.randint(100000, 999999)
+        expire_date = datetime.now() + timedelta(minutes=10)
+
+        db.reference(f"/password_reset/{user.uid}").set({
+            "code": reset_code,
+            "expires": expire_date.timestamp()
+        })
+
+        print(f"Code: {reset_code}")
+        print(f"Expires: {expire_date.timestamp()}")
+
+        return reset_code
+
+    async def get_verification_code(self, uid: str, code: int) -> VerifyResetCode:
+        user = self.user_repo.get_by_uid(uid)
+        if user is None:
+            return None
+
+        code_data = db.reference(f"/password_reset/{user.uid}/").get()
+
+        if not code_data:
+            return None
+
+        verify_reset_code = VerifyResetCode(uid=user.uid, code=int(
+            code_data.get("code")), exp=code_data.get("expires"))
+
+        expire = datetime.fromtimestamp(verify_reset_code.exp)
+
+        if datetime.now() > expire:
+            return None
+
+        if verify_reset_code.code != code:
+            return None
+
+        return VerifyResetCode(uid=user.uid, code=code_data.get("code"), exp=code_data.get("expires"))
+
+    async def verify_reset_code(self, email: str, code: int) -> VerifyResetCode:
+        return await self.get_verification_code(uid=self.user_repo.get_by_email(email).uid, code=code)
+
+    async def change_password(self, uid: str, new_password: str):
+
+        user = self.user_repo.change_password(
+            uid=uid, password=new_password)
+
+        db.reference(f"/password_reset/{user.uid}/").delete()
+
+        return user
