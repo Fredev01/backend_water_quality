c3f4000542aa5e91043a341b1a4010ddf3f989b6
EmmanuelOZ <davidozuna84@hotmail.com>
2025-07-16 22:10:07 -0600

    feat: implementar manejo de errores personalizados en el servicio de autenticación y envío de correos

--- app/features/auth/services/services.py
+++ app/features/auth/services/services.py
@@ -1,14 +1,14 @@
-from fastapi import HTTPException
 import random
 from datetime import datetime, timedelta
 from firebase_admin import auth, db
 import httpx
-from app.features.auth.domain.body import UpdatePassword
+from app.features.auth.domain.errors import AuthError
 from app.features.auth.domain.model import GenerateResetCode, VerifyResetCode
 from app.share.firebase.domain.config import FirebaseConfigImpl
 from app.share.users.domain.enum.roles import Roles
+from app.share.users.domain.errors import UserError
 from app.share.users.domain.model.auth import UserLogin, UserRegister
-from app.share.users.domain.model.user import UserData, UserDetail
+from app.share.users.domain.model.user import UserData
 from app.share.users.domain.repository import UserRepository
 
 
@@ -21,7 +21,6 @@ class AuthService:
         return user
 
     async def login(self, user: UserLogin) -> UserData:
-
         try:
             config = FirebaseConfigImpl()
             api_key = config.api_key
@@ -40,41 +39,18 @@ class AuthService:
                 response = await client.post(url_sign_in, json=body)
 
                 if response.status_code != 200:
-                    raise HTTPException(
-                        status_code=400, detail="Invalid credentials")
+                    raise AuthError(
+                        status_code=response.status_code, message="Credenciales inválidas")
 
                 return auth_user
 
         except auth.UserNotFoundError:
-            raise HTTPException(status_code=401, detail="Unregistered user")
-        except Exception as e:
-            print(e.__class__.__name__)
-            print(e)
-            raise HTTPException(status_code=500, detail="Server error")
-
-    async def send_reset_password(self, email: str) -> str:
-        config = FirebaseConfigImpl()
-        api_key = config.api_key
-        url_reset = f'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key={api_key}'
-
-        body = {
-            "requestType": "PASSWORD_RESET",
-            "email": email
-        }
-
-        async with httpx.AsyncClient() as client:
-            response = await client.post(url_reset, json=body)
-
-            if response.status_code != 200:
-                raise HTTPException(
-                    status_code=400, detail="Failed to send reset email")
-
-            return {"message": "Password reset email sent"}
+            raise UserError(status_code=401, message="Usuario no registrado")
 
     async def generate_verification_code(self, email: str) -> GenerateResetCode:
         user = self.user_repo.get_by_email(email)
         if user is None:
-            raise HTTPException(status_code=404, detail="User not found")
+            raise UserError(status_code=404, message="Usuario no registrado")
 
         reset_code = random.randint(100000, 999999)
         expire_date = datetime.now() + timedelta(minutes=10)
@@ -89,15 +65,18 @@ class AuthService:
 
         return GenerateResetCode(username=user.username, code=reset_code)
 
-    async def get_verification_code(self, uid: str, code: int) -> VerifyResetCode:
-        user = self.user_repo.get_by_uid(uid)
+    async def get_verification_code(self, uid: str, code: int, user: UserData = None) -> VerifyResetCode:
+
+        user = user or self.user_repo.get_by_uid(uid)
+
         if user is None:
-            return None
+            raise AuthError(status_code=404, message="Usuario no registrado")
 
         code_data = db.reference(f"/password_reset/{user.uid}/").get()
 
         if not code_data:
-            return None
+            raise AuthError(status_code=401,
+                            message="Código de verificación inválido")
 
         verify_reset_code = VerifyResetCode(uid=user.uid, code=int(
             code_data.get("code")), exp=code_data.get("expires"))
@@ -105,15 +84,24 @@ class AuthService:
         expire = datetime.fromtimestamp(verify_reset_code.exp)
 
         if datetime.now() > expire:
-            return None
+            raise AuthError(status_code=401, message="Código expirado")
 
         if verify_reset_code.code != code:
-            return None
+            raise AuthError(status_code=401,
+                            message="Código de verificación inválido")
 
         return VerifyResetCode(uid=user.uid, code=code_data.get("code"), exp=code_data.get("expires"))
 
     async def verify_reset_code(self, email: str, code: int) -> VerifyResetCode:
-        return await self.get_verification_code(uid=self.user_repo.get_by_email(email).uid, code=code)
+        try:
+            user = self.user_repo.get_by_email(email)
+            if user is None:
+                raise UserError(status_code=404,
+                                message="Usuario no registrado")
+
+            return await self.get_verification_code(uid=user.uid, code=code, user=user)
+        except UserError as ue:
+            raise AuthError(status_code=ue.status_code, message=ue.message)
 
     async def change_password(self, uid: str, new_password: str):
 
--- app/share/email/service/resend_email.py
+++ app/share/email/service/resend_email.py
@@ -1,12 +1,14 @@
 import resend
+from resend.exceptions import ApplicationError
 from app.share.email.domain.config import EmailConfig
+from app.share.email.domain.errors import EmailSeedError
 from app.share.email.domain.repo import EmailRepository
 
 
 class ResendEmailService(EmailRepository):
     config: EmailConfig = EmailConfig()
 
-    def send(self, to, subject, body):
+    def send(self, to, subject, body, raise_error=False):
         try:
             resend.api_key = self.config.api_key
 
@@ -17,6 +19,12 @@ class ResendEmailService(EmailRepository):
                 "html": body
             }
             resend.Emails.send(params)
+        except ApplicationError as e:
+            print(e.__class__.__name__)
+            print(e)
+
+            if raise_error:
+                raise EmailSeedError(e.message, e.status_code)
         except Exception as e:
             print(e.__class__.__name__)
             print(e)
